8cef0707fa26f852cd477342024c839d1e22f2a2
Elijovial <omeehelijah345@gmail.com>
2023-11-25 03:41:38 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
-# Install flask from pip3 using Puppet
-
-# Include the python module
-include python
-
-# Install flask with pip3
-package { 'Flask':
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
-# Install Werkzeug
-package { 'Werkzeug':
- ensure	   => '2.1.1',
- provider  => 'pip3'
+  provider => pip3,
 }
